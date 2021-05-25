7e94730d3d9d35b2e91851842ed9edb90b77b89a
Tomhauscz <hak.tomas11@seznam.cz>
2021-05-25 21:25:29 +0200

    KETCube package release!

--- KETCube_arduino_package/tools/arm-none-eabi-gcc/8.2.1-1.7/gnu-mcu-eclipse/scripts/git-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rm -rf "${HOME}/Downloads/arm-none-eabi-gcc-build.git"
-git clone --recurse-submodules https://github.com/gnu-mcu-eclipse/arm-none-eabi-gcc-build.git "${HOME}/Downloads/arm-none-eabi-gcc-build.git"
